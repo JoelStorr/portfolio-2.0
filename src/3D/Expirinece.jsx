75d763efb3495bfe4541ad54cb3121fb149bac66
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-07-13 19:40:37 +0200

    Clean up

--- src/3D/Expirinece.jsx
+++ src/3D/Expirinece.jsx
@@ -1,78 +1,56 @@
-import { Environment, Html, Stage } from '@react-three/drei';
-import React, { useEffect, useState } from 'react'
-import Desktop from '../Pages/Desktop/Desktop';
-import TestComponent from './TestComponent';
+import { Environment, Html, Stage } from "@react-three/drei";
+import React, { useEffect, useState } from "react";
+import Desktop from "../Pages/Desktop/Desktop";
+import TestComponent from "./TestComponent";
 
-import './Expirience.css';
+import "./Expirience.css";
 
 export default function Expirinece() {
+  const [path, setPath] = useState(window.location.pathname);
 
-
-
-  const [path, setPath] = useState(window.location.pathname)
-
-
-  useEffect(()=>{
-
-    const handleClicks = ()=>{
-
+  useEffect(() => {
+    const handleClicks = () => {
       setPath(window.location.pathname);
-    }
+    };
 
-    document.body.addEventListener('click', handleClicks)
-    window.addEventListener('popstate', handleClicks)
-    
+    document.body.addEventListener("click", handleClicks);
+    window.addEventListener("popstate", handleClicks);
+
     console.log(path);
 
-    return ()=>{
-      document.body.removeEventListener('click', handleClicks);
-      window.removeEventListener('popstate', handleClicks);
-    }
+    return () => {
+      document.body.removeEventListener("click", handleClicks);
+      window.removeEventListener("popstate", handleClicks);
+    };
+  });
 
-  })
-
-
-
-  if(path === '/gallary'){
-
+  if (path === "/gallary") {
     return (
       <Stage>
         <mesh>
-          <boxGeometry/>
-          <meshNormalMaterial/>
+          <boxGeometry />
+          <meshNormalMaterial />
         </mesh>
       </Stage>
-    )
-
+    );
   } else {
-
     return (
       <>
-          <Stage 
-             shadows={{type: 'contact', opacity: 0.2, blur:3}} 
-             environment="sunset"
-             preset='portrait'
-             intensity={2}
-             
-          >
-          <mesh
-              position-z={-5}
-              receiveShadow={true}
-              castShadow={true}
-          >
-          <torusKnotGeometry />
-          <meshLambertMaterial />
-          <Html position-z={3}  center className='html-holder'>
+        <Stage
+          shadows={{ type: "contact", opacity: 0.2, blur: 3 }}
+          environment="sunset"
+          preset="portrait"
+          intensity={2}
+        >
+          <mesh position-z={-5} receiveShadow={true} castShadow={true}>
+            <torusKnotGeometry />
+            <meshLambertMaterial />
+            <Html position-z={3} center className="html-holder">
               <Desktop />
-          </Html>
+            </Html>
           </mesh>
-          </Stage>
+        </Stage>
       </>
-  
-      
-  
     );
   }
-
-
 }
